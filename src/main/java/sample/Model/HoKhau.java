ee18e18e4250122e6b8d3360199669225725f22b
HuyLong2k4 <maihuylong102@gmail.com>
2025-05-30 23:27:17 +0700

    123456

--- src/main/java/sample/Model/HoKhau.java
+++ src/main/java/sample/Model/HoKhau.java
@@ -5,7 +5,7 @@ import javafx.beans.property.*;
 public class HoKhau {
     private IntegerProperty soHoKhau;
     private StringProperty soNha;
-    private StringProperty duong;git
+    private StringProperty duong;
     private StringProperty phuong;
     private StringProperty quan;
     private ObjectProperty<java.time.LocalDate> ngayLamHoKhau;
--- src/main/java/sample/Model/KhoanThu.java
+++ src/main/java/sample/Model/KhoanThu.java
@@ -1,46 +1,65 @@
 package main.java.sample.Model;
 
-import javafx.beans.property.*;
 import java.time.LocalDate;
 
 public class KhoanThu {
-    private IntegerProperty id;
-    private ObjectProperty<LocalDate> ngayTao;
-    private ObjectProperty<LocalDate> thoiHan;
-    private StringProperty tenKhoanThu;
-    private BooleanProperty batBuoc;
-    private StringProperty ghiChu;
+    private int id;
+    private LocalDate ngayTao;
+    private LocalDate thoiHan;
+    private String tenKhoanThu;
+    private boolean batBuoc;
+    private String ghiChu;
 
     public KhoanThu() {
-        this.id = new SimpleIntegerProperty();
-        this.ngayTao = new SimpleObjectProperty<>();
-        this.thoiHan = new SimpleObjectProperty<>();
-        this.tenKhoanThu = new SimpleStringProperty();
-        this.batBuoc = new SimpleBooleanProperty();
-        this.ghiChu = new SimpleStringProperty();
+        // Constructor mặc định
     }
 
-    public int getId() { return id.get(); }
-    public void setId(int id) { this.id.set(id); }
-    public IntegerProperty idProperty() { return id; }
+    // Getter và Setter
+    public int getId() {
+        return id;
+    }
 
-    public LocalDate getNgayTao() { return ngayTao.get(); }
-    public void setNgayTao(LocalDate ngayTao) { this.ngayTao.set(ngayTao); }
-    public ObjectProperty<LocalDate> ngayTaoProperty() { return ngayTao; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public LocalDate getThoiHan() { return thoiHan.get(); }
-    public void setThoiHan(LocalDate thoiHan) { this.thoiHan.set(thoiHan); }
-    public ObjectProperty<LocalDate> thoiHanProperty() { return thoiHan; }
+    public LocalDate getNgayTao() {
+        return ngayTao;
+    }
 
-    public String getTenKhoanThu() { return tenKhoanThu.get(); }
-    public void setTenKhoanThu(String tenKhoanThu) { this.tenKhoanThu.set(tenKhoanThu); }
-    public StringProperty tenKhoanThuProperty() { return tenKhoanThu; }
+    public void setNgayTao(LocalDate ngayTao) {
+        this.ngayTao = ngayTao;
+    }
 
-    public boolean isBatBuoc() { return batBuoc.get(); }
-    public void setBatBuoc(boolean batBuoc) { this.batBuoc.set(batBuoc); }
-    public BooleanProperty batBuocProperty() { return batBuoc; }
+    public LocalDate getThoiHan() {
+        return thoiHan;
+    }
 
-    public String getGhiChu() { return ghiChu.get(); }
-    public void setGhiChu(String ghiChu) { this.ghiChu.set(ghiChu); }
-    public StringProperty ghiChuProperty() { return ghiChu; }
+    public void setThoiHan(LocalDate thoiHan) {
+        this.thoiHan = thoiHan;
+    }
+
+    public String getTenKhoanThu() {
+        return tenKhoanThu;
+    }
+
+    public void setTenKhoanThu(String tenKhoanThu) {
+        this.tenKhoanThu = tenKhoanThu;
+    }
+
+    public boolean isBatBuoc() {
+        return batBuoc;
+    }
+
+    public void setBatBuoc(boolean batBuoc) {
+        this.batBuoc = batBuoc;
+    }
+
+    public String getGhiChu() {
+        return ghiChu;
+    }
+
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
 }
